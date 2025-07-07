f35d9cc21c8b7b7c79067544aa2ced628b92092c
bisal-xetri <bishaldkl56@gmail.com>
2025-07-07 12:39:18 +0545

    implement menu section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Hero from "./components/Hero";
 import Cocktail from "./components/Cocktail";
 import About from "./components/About";
 import Art from "./components/Art";
+import Menu from "./components/Menu";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 const App = () => {
@@ -15,6 +16,7 @@ const App = () => {
       <Cocktail />
       <About />
       <Art />
+      <Menu />
     </main>
   );
 };
